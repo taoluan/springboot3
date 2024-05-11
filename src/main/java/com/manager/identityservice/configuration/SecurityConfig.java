7d846cb86bce2b32b3c040096ab0e59c94beb802
taoluan.dev <tvanluanst@gmail.com>
2024-05-11 22:56:25 +0700

    feat: Spring security architecture authentication with JWT

--- src/main/java/com/manager/identityservice/configuration/SecurityConfig.java
+++ src/main/java/com/manager/identityservice/configuration/SecurityConfig.java
@@ -1,10 +1,79 @@
 package com.manager.identityservice.configuration;
 
+import com.manager.identityservice.enums.Role;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.oauth2.jose.jws.MacAlgorithm;
+import org.springframework.security.oauth2.jwt.JwtDecoder;
+import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
+import org.springframework.security.oauth2.server.resource.authentication.JwtGrantedAuthoritiesConverter;
+import org.springframework.security.web.SecurityFilterChain;
 
+import javax.crypto.spec.SecretKeySpec;
+
+@Configuration
+@EnableWebSecurity
 public class SecurityConfig {
+
+    private final String[] PUBLIC_ENDPOINT = {"users", "auth/login", "auth/introspect"};
+
+    private final String[] ROLE_ADMIN_ENDPOINT = {"users"};
+
+    @Value("${jwt.signerKey}")
+    private String signerKey;
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception {
+        // config public routes api
+        httpSecurity.authorizeHttpRequests(request ->
+                request.requestMatchers(HttpMethod.POST, PUBLIC_ENDPOINT).permitAll()
+
+                        .requestMatchers(HttpMethod.GET, ROLE_ADMIN_ENDPOINT)
+                        .hasRole(Role.ADMIN.name())
+
+                        .anyRequest()
+                        .authenticated());
+
+        // config authenticate with jwt
+        httpSecurity.oauth2ResourceServer(oauth2 ->
+                oauth2.jwt(jwtConfigurer -> jwtConfigurer.decoder(jwtDecoder())
+                        .jwtAuthenticationConverter(jwtAuthenticationConverter()))
+        );
+
+        // disable csrf
+        httpSecurity.csrf(AbstractHttpConfigurer::disable);
+
+        return httpSecurity.build();
+    }
+
+    @Bean
+    JwtAuthenticationConverter jwtAuthenticationConverter() {
+        // Changed prefix claim default scope to role: "SCOPE_ADMIN" -> "ROLE_ADMIN"
+        JwtGrantedAuthoritiesConverter jwtGrantedAuthoritiesConverter = new JwtGrantedAuthoritiesConverter();
+        jwtGrantedAuthoritiesConverter.setAuthorityPrefix("ROLE_");
+
+        JwtAuthenticationConverter jwtAuthenticationConverter = new JwtAuthenticationConverter();
+        jwtAuthenticationConverter.setJwtGrantedAuthoritiesConverter(jwtGrantedAuthoritiesConverter);
+
+        return jwtAuthenticationConverter;
+    }
+
+    @Bean
+    JwtDecoder jwtDecoder() {
+        SecretKeySpec secretKeySpec = new SecretKeySpec(signerKey.getBytes(), "HS512");
+        return NimbusJwtDecoder.withSecretKey(secretKeySpec)
+                .macAlgorithm(MacAlgorithm.HS512)
+                .build();
+    }
 
     @Bean
     PasswordEncoder passwordEncoder() {
